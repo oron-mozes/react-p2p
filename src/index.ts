8af8aa3908907a6c5c7da81812037fc35163dbd9
oron.mozes <oron.mozes@khealth.com>
2023-12-01 14:24:23 +0200

    update pkg name

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
+import {
+  NativeModulesProxy,
+  EventEmitter,
+  Subscription,
+} from "expo-modules-core";
 
 // Import the native module. On web, it will be resolved to ReactP2p.web.ts
 // and on native platforms to ReactP2p.ts
-import ReactP2pModule from './ReactP2pModule';
-import ReactP2pView from './ReactP2pView';
-import { ChangeEventPayload, ReactP2pViewProps } from './ReactP2p.types';
+import ReactP2pModule from "./ReactP2pModule";
+import ReactP2pView from "./ReactP2pView";
+import { ChangeEventPayload, ReactP2pViewProps } from "./ReactP2p.types";
 
 // Get the native constant value.
 export const PI = ReactP2pModule.PI;
@@ -19,8 +23,10 @@ export async function setValueAsync(value: string) {
 
 const emitter = new EventEmitter(ReactP2pModule ?? NativeModulesProxy.ReactP2p);
 
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
+export function addChangeListener(
+  listener: (event: ChangeEventPayload) => void
+): Subscription {
+  return emitter.addListener<ChangeEventPayload>("onChange", listener);
 }
 
 export { ReactP2pView, ReactP2pViewProps, ChangeEventPayload };
